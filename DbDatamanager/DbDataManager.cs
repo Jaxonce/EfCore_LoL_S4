674d841281f5f9fb57c522ccf9d1a34ea6788045
Maxence Lanone <maxence.lanone@etu.uca.fr>
2023-01-31 23:27:51 +0100

    ajout lib

---
+++ DbDatamanager/DbDataManager.cs
@@ -0,0 +1,13 @@
+﻿using Model;
+
+namespace DbDatamanager;
+public class DbDataManager : IDataManager
+{
+    public IChampionsManager ChampionsMgr => throw new NotImplementedException();
+
+    public ISkinsManager SkinsMgr => throw new NotImplementedException();
+
+    public IRunesManager RunesMgr => throw new NotImplementedException();
+
+    public IRunePagesManager RunePagesMgr => throw new NotImplementedException();
+}
