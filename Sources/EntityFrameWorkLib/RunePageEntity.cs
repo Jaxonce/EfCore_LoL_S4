8268a713fd79503c030d528a8ed4d6ca7e0a4866
Maxence Lanone <maxence.lanone@etu.uca.fr>
2023-03-26 21:36:38 +0200

    ajout test unit add in memory en inline data + modif champ en fact

---
+++ Sources/EntityFrameWorkLib/RunePageEntity.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace EntityFrameWorkLib
+{
+	public class RunePageEntity
+	{
+        public int Id { get; set; }
+        public String Name { get; set; }
+    }
+
+    public enum Category
+    {
+        Major,
+        Minor1,
+        Minor2,
+        Minor3,
+        OtherMinor1,
+        OtherMinor2
+    }
+}
+
---
+++ Sources/Tests/TestUnitaireLOL/TestEf.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using EntityFrameWorkLib;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using Model;
+using Shared;
+
+namespace TestUnitaireLOL
+{
+	public class TestsData
+	{
+        public IEnumerable<object[]> Data_TestAddItem()
+        {
+            yield return new object[]
+            {
+                1,
+                new ChampionEntity[]
+                {
+                    new ChampionEntity
+                    {
+                        Name = "jax",
+                        Icon = "icon jax",
+                        Bio = "test bio jax",
+                        Class = ChampionClass.Fighter,
+                        LargeImageEntity = new LargeImageEntity(){Base64 = "tevhdfvsdefefef"}
+                    },
+                    new ChampionEntity
+                    {
+                        Name = "jax2",
+                        Icon = "icon jax2",
+                        Bio = "test bio jax2",
+                        Class = ChampionClass.Assassin,
+                        LargeImageEntity = new LargeImageEntity(){Base64 = "tevhdfvsdfefefe"}
+                    },
+                    new ChampionEntity
+                    {
+                        Name = "jax3",
+                        Icon = "icon jax3",
+                        Bio = "test bio jax3",
+                        Class = ChampionClass.Tank,
+                        LargeImageEntity = new LargeImageEntity(){Base64 = "tevhdfvsdfkhefef"}
+                    }
+                },
+                new ChampionEntity[]
+                {
+                    new ChampionEntity()
+                    {
+                        Name = "jax4",
+                        Icon = "icon jax4",
+                        Bio = "test bio jax4",
+                        Class = ChampionClass.Marksman,
+                        LargeImageEntity = new LargeImageEntity(){Base64 = "tevhdfvsdfkhvfdfdf"}
+                    }
+                }
+            };
+
+        }
+    }
+}
+
